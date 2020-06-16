a53cade3169439263c272e37b8b3c3402a205770
Jared Galloway <jaredgalloway07@gmail.com>
2020-06-16 09:05:33 -0700

    Dynamic alignment schemes (#10)
    * dymanic alignment

--- run_local.sh
+++ run_local.sh
@@ -5,9 +5,7 @@ nextflow \
     -C "nextflow.local.config" \
     run \
     main.nf \
-    --params_file simulations/simulate_ones_config.json \
+    --params_file simulations/simulate_ones_single_ref.json \
     -with-report output/nextflow_report.html \
     -work-dir output/work/ \
     -ansi-log false \
-    -with-dag dag.pdf \
-    -resume

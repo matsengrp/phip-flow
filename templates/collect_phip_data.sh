a938e3497e4a5f5fa91261ce1f6347e0396da325
Sam Minot <sminot@fredhutch.org>
2021-11-10 10:10:11 -0800

    Potential DSL-2 Refactor (#25)
    * Git: Ignore VSCode workspace files
    
    * Untested DSL-2 refactor
    
    * changed shell block -> script for templates
    
    Co-authored-by: Jared Galloway <jaredgalloway07@gmail.com>

---
+++ templates/collect_phip_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Running phipflow"
+
+phipflow load-from-counts-tsv \
+    --sample_table ${sample_table} \
+    --peptide_table ${peptide_table} \
+    -c '*.counts' \
+    -s '*.stats' \
+    -o ${params.dataset_prefix}.phip
+
+echo "Done"

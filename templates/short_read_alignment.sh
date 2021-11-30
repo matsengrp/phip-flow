0bf2daa42710db1917d64933a60cd84f7b553590
Jared Galloway <jaredgalloway07@gmail.com>
2021-11-30 07:47:55 -0800

    updated alignment config params (#33)

--- templates/short_read_alignment.sh
+++ templates/short_read_alignment.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
+: '
+This template aligns reads to the index after trimming the 
+read to be the same length as tiles in the library.
+For more on bowtie alignment, see the options
+documented by bowtie.
+
+We report only the best alignment found, and no more.
+
+You may specify the number of allowed mismatches in the confi file
+or hard-code it (or any other options) here.
+'
+
 set -euo pipefail
 
 STREAM_FILE_CMD=!{params.fastq_stream_func}
 FASTQ=!{respective_replicate_path}
 INDEX=!{index}/peptide
 ALIGN_OUT_FN=!{sample_id}.sam
+READ_LENGTH=!{params.read_length}
+PEPTIDE_LENGTH=!{params.peptide_tile_length}
+CPUS=!{task.cpus}
+MM=!{params.n_mismatches}
+
+if [ ${PEPTIDE_LENGTH} -lt ${READ_LENGTH} ]; then
+    let TRIM3=${READ_LENGTH}-${PEPTIDE_LENGTH}
+else
+    TRIM3=0
+fi
 
 $STREAM_FILE_CMD $FASTQ | bowtie \
-  --trim3 8 \
-  --threads 4 \
-  -n 2 \
-  -l 117 \
+  --trim3 $TRIM3 \
+  --threads $CPUS \
+  -n $MM \
+  -l $PEPTIDE_LENGTH \
   --tryhard \
   --nomaqround \
   --norc \
